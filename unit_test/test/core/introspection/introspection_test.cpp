8c6b15c44634636a371e5d4f1781f13b07ba1cfe
Jeffrey Hellrung <jeffrey.hellrung@gmail.com>
2011-09-11 22:43:26 -0700

    updating introspection test

--- unit_test/test/core/introspection/introspection_test.cpp
+++ unit_test/test/core/introspection/introspection_test.cpp
@@ -8,10 +8,14 @@
 
 #include <sake/core/introspection/has_operator_ampersand.hpp>
 #include <sake/core/introspection/has_operator_bit_and.hpp>
+#include <sake/core/introspection/has_operator_bit_and_assign.hpp>
 #include <sake/core/introspection/has_operator_bit_or.hpp>
+#include <sake/core/introspection/has_operator_bit_or_assign.hpp>
 #include <sake/core/introspection/has_operator_bit_xor.hpp>
+#include <sake/core/introspection/has_operator_bit_xor_assign.hpp>
 #include <sake/core/introspection/has_operator_complement.hpp>
 #include <sake/core/introspection/has_operator_divides.hpp>
+#include <sake/core/introspection/has_operator_divides_assign.hpp>
 #include <sake/core/introspection/has_operator_equal.hpp>
 #include <sake/core/introspection/has_operator_greater.hpp>
 #include <sake/core/introspection/has_operator_greater_equal.hpp>
@@ -21,16 +25,22 @@
 #include <sake/core/introspection/has_operator_logical_not.hpp>
 #include <sake/core/introspection/has_operator_logical_or.hpp>
 #include <sake/core/introspection/has_operator_minus.hpp>
+#include <sake/core/introspection/has_operator_minus_assign.hpp>
 #include <sake/core/introspection/has_operator_modulus.hpp>
+#include <sake/core/introspection/has_operator_modulus_assign.hpp>
 #include <sake/core/introspection/has_operator_multiplies.hpp>
+#include <sake/core/introspection/has_operator_multiplies_assign.hpp>
 #include <sake/core/introspection/has_operator_not_equal.hpp>
 #include <sake/core/introspection/has_operator_plus.hpp>
+#include <sake/core/introspection/has_operator_plus_assign.hpp>
 #include <sake/core/introspection/has_operator_post_decrement.hpp>
 #include <sake/core/introspection/has_operator_post_increment.hpp>
 #include <sake/core/introspection/has_operator_pre_decrement.hpp>
 #include <sake/core/introspection/has_operator_pre_increment.hpp>
 #include <sake/core/introspection/has_operator_shift_left.hpp>
+#include <sake/core/introspection/has_operator_shift_left_assign.hpp>
 #include <sake/core/introspection/has_operator_shift_right.hpp>
+#include <sake/core/introspection/has_operator_shift_right_assign.hpp>
 #include <sake/core/introspection/has_operator_star.hpp>
 #include <sake/core/introspection/has_operator_unary_minus.hpp>
 #include <sake/core/introspection/has_operator_unary_plus.hpp>
