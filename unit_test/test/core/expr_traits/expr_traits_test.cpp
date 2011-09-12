c245b333933774248cfda59ddbf146e2c344410d
Jeffrey Hellrung <jeffrey.hellrung@gmail.com>
2011-09-11 23:30:09 -0700

    adding expr_traits compile test

---
+++ unit_test/test/core/expr_traits/expr_traits_test.cpp
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * unit_test/test/core/expr_traits/expr_traits_test.cpp
+ *
+ * Copyright 2011, Jeffrey Hellrung.
+ * Distributed under the Boost Software License, Version 1.0.  (See accompanying
+ * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ ******************************************************************************/
+
+#include <sake/core/expr_traits/is_convertible.hpp>
+#include <sake/core/expr_traits/is_rvalue.hpp>
+#include <sake/core/expr_traits/is_void.hpp>
+#include <sake/core/expr_traits/type_tag_of.hpp>
+
+#include <sake/test/environment.hpp>
+
+namespace sake_unit_test
+{
+
+void expr_traits_test(sake::test::environment& env)
+{ }
+
+} // namespace sake_unit_test
