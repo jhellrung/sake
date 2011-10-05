f335d0555e1b884d971a378f9ab42c8754628936
Jeffrey Hellrung <jeffrey.hellrung@gmail.com>
2011-10-04 23:19:21 -0700

    renaming some decl* functions

--- sake/core/utility/declval.hpp
+++ sake/core/utility/declval.hpp
@@ -5,10 +5,16 @@
  * Distributed under the Boost Software License, Version 1.0.  (See accompanying
  * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  *
- * declval<T>()       -> T
- * declval_const<T>() -> boost::add_const<T>::type
- * declval_ref<T>()   -> boost_ext::add_reference<T>::type
- * declval_cref<T>()  -> boost_ext::add_reference_add_const<T>::type
+ * declval<T>()  -> T
+ *
+ * declcval<T>() -> boost::add_const<T>::type
+ * declvval<T>() -> boost::add_volatile<T>::type
+ * declcvval<T>() -> boost::add_cv<T>::type
+ *
+ * declref<T>()  -> boost_ext::add_reference<T>::type
+ * declcref<T>() -> boost_ext::add_reference_add_const<T>::type
+ * declvref<T>() -> boost_ext::add_reference_add_volatile<T>::type
+ * declcvref<T>() -> boost_ext::add_reference_add_cv<T>::type
  *
  * declval can be used to syntactically represent an "rvalue correct" instance
  * of a type.
@@ -20,17 +26,27 @@
 #define SAKE_CORE_UTILITY_DECLVAL_HPP
 
 #include <boost/type_traits/add_const.hpp>
+#include <boost/type_traits/add_cv.hpp>
+#include <boost/type_traits/add_volatile.hpp>
 
 #include <sake/boost_ext/type_traits/add_reference.hpp>
 #include <sake/boost_ext/type_traits/add_reference_add_const.hpp>
+#include <sake/boost_ext/type_traits/add_reference_add_cv.hpp>
+#include <sake/boost_ext/type_traits/add_reference_add_volatile.hpp>
 
 namespace sake
 {
 
 template< class T > T declval();
-template< class T > typename boost::add_const<T>::type declval_const();
-template< class T > typename boost_ext::add_reference<T>::type declval_ref();
-template< class T > typename boost_ext::add_reference_add_const<T>::type declval_cref();
+
+template< class T > typename boost::add_const<T>::type declcval();
+template< class T > typename boost::add_volatile<T>::type declvval();
+template< class T > typename boost::add_cv<T>::type declcvval();
+
+template< class T > typename boost_ext::add_reference<T>::type declref();
+template< class T > typename boost_ext::add_reference_add_const<T>::type declcref();
+template< class T > typename boost_ext::add_reference_add_volatile<T>::type declvref();
+template< class T > typename boost_ext::add_reference_add_cv<T>::type declcvref();
 
 } // namespace sake
 
