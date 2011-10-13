5e468dda5c48dd31723a5e9df5c55974b96b2e4c
Jeffrey Hellrung <jeffrey.hellrung@gmail.com>
2011-10-13 15:46:21 -0700

    reimplemnting sake::abs

--- sake/core/math/abs.hpp
+++ sake/core/math/abs.hpp
@@ -31,7 +31,6 @@
 
 #include <cmath>
 
-#include <boost/config.hpp>
 #include <boost/type_traits/is_signed.hpp>
 #include <boost/type_traits/is_unsigned.hpp>
 #include <boost/type_traits/make_unsigned.hpp>
@@ -40,6 +39,7 @@
 #include <sake/boost_ext/type_traits/remove_qualifiers.hpp>
 
 #include <sake/core/functional/operators/unary_minus.hpp>
+#include <sake/core/introspection/is_callable_function.hpp>
 #include <sake/core/introspection/is_callable_member_function.hpp>
 #include <sake/core/math/abs_fwd.hpp>
 #include <sake/core/math/zero.hpp>
@@ -48,72 +48,13 @@
 namespace sake
 {
 
-#define SAKE_INTROSPECTION_TRAIT_NAME           is_callable_mem_fun_abs
-#define SAKE_INTROSPECTION_MEMBER_FUNCTION_NAME abs
-#define SAKE_INTROSPECTION_MEMBER_FUNCTION_DEFAULT_SIGNATURE( T ) \
-    typename boost_ext::remove_qualifiers<T>::type ( )
-#define SAKE_INTROSPECTION_MEMBER_FUNCTION_ARITY_LIMITS ( 0, 0 )
-#include SAKE_INTROSPECTION_DEFINE_IS_CALLABLE_MEMBER_FUNCTION()
-
-namespace default_impl
-{
-
 namespace abs_private
 {
 
-template<
-    class T,
-    bool = sake::is_callable_mem_fun_abs< T const & >::value,
-    bool = boost::is_signed<T>::value,
-    bool = boost::is_unsigned<T>::value
->
-struct dispatch;
-
 template< class T >
-struct dispatch< T, true, false, false >
-{
-    typedef T type;
-    static type apply(T const & x)
-    { return x.abs(); }
-};
-
-template< class T >
-struct dispatch< T, false, true, false >
-{
-    typedef typename boost::make_unsigned<T>::type type;
-    static type apply(T const x)
-    {
-#ifdef _MSC_VER
-#pragma warning( push )
-#pragma warning( disable : 4146 ) // unary minus operator applied to unsigned type, result still unsigned
-#endif // #ifdef _MSC_VER
-        return x < 0 ? -static_cast< type >(x) : static_cast< type >(x);
-#ifdef _MSC_VER
-#pragma warning( pop )
-#endif // #ifdef _MSC_VER
-    }
-};
-
-template< class T >
-struct dispatch< T, false, false, true >
-{
-    typedef T type;
-    static type apply(T const x)
-    { return x; }
-};
-
-template< class T >
-struct dispatch< T, false, false, false >
-{
-    typedef typename boost_ext::common_type<
-        typename operators::result_of::unary_minus<T>::type, T >::type type;
-    static type apply(T const & x)
-    { return x < sake::zero ? -x : x; }
-};
+struct impl;
 
 } // namespace abs_private
-
-} // namespace default_impl
 
 namespace result_of
 {
@@ -152,7 +93,7 @@ namespace default_impl
 
 template< class T >
 struct abs
-    : sake::default_impl::abs_private::dispatch<
+    : abs_private::impl<
           typename boost_ext::remove_qualifiers<T>::type
       >
 { };
@@ -160,46 +101,6 @@ struct abs
 } // namespace default_impl
 
 } // namespace result_of
-
-namespace default_impl
-{
-
-template< class T >
-inline typename abs_private::dispatch<T>::type
-abs(T const & x)
-{ return abs_private::dispatch<T>::apply(x); }
-
-inline float
-abs(float const x)
-{ return std::fabs(x); }
-
-inline double
-abs(double const x)
-{ return std::fabs(x); }
-
-inline long double
-abs(long double const x)
-{ return std::fabs(x); }
-
-} // namespace default_impl
-
-} // namespace sake
-
-namespace sake_abs_private
-{
-
-template< class T >
-inline typename ::sake::result_of::abs<T>::type
-impl(T const & x)
-{
-    using ::sake::default_impl::abs;
-    return abs(x);
-}
-
-} // namespace sake_abs_private
-
-namespace sake
-{
 
 /*******************************************************************************
  * abs(T const & x) -> result_of::abs<T>::type
@@ -216,21 +117,7 @@ struct abs
     template< class T >
     typename result_of::abs<T>::type
     operator()(T const & x) const
-    { return ::sake_abs_private::impl(x); }
-
-    unsigned int
-    operator()(int const x) const
-    { return default_impl::abs(x); }
-
-    unsigned long
-    operator()(long const x) const
-    { return default_impl::abs(x); }
-
-    #ifndef BOOST_NO_LONG_LONG
-    unsigned long long
-    operator()(long long const x) const
-    { return default_impl::abs(x); }
-    #endif // #ifndef BOOST_NO_LONG_LONG
+    { sake::abs_private::impl<T>::apply(x); }
 
     float
     operator()(float const x) const
@@ -246,6 +133,122 @@ struct abs
 } // namespace functional
 
 functional::abs const abs = { };
+
+} // namespace sake
+
+namespace sake_abs_private
+{
+
+#define SAKE_INTROSPECTION_TRAIT_NAME    is_callable
+#define SAKE_INTROSPECTION_FUNCTION_NAME abs
+#define SAKE_INTROSPECTION_FUNCTION_ARITY_LIMITS ( 1, 1 )
+#include SAKE_INTROSPECTION_DEFINE_IS_CALLABLE_FUNCTION()
+
+template< class T >
+inline typename ::sake::result_of::abs<T>::type
+adl(T const & x)
+{ return abs(x); }
+
+} // namespace sake_abs_private
+
+namespace sake
+{
+
+namespace abs_private
+{
+
+#define SAKE_INTROSPECTION_TRAIT_NAME           is_callable_mem_fun
+#define SAKE_INTROSPECTION_MEMBER_FUNCTION_NAME abs
+#define SAKE_INTROSPECTION_MEMBER_FUNCTION_DEFAULT_SIGNATURE( T ) \
+    typename boost_ext::remove_qualifiers<T>::type ( )
+#define SAKE_INTROSPECTION_MEMBER_FUNCTION_ARITY_LIMITS ( 0, 0 )
+#include SAKE_INTROSPECTION_DEFINE_IS_CALLABLE_MEMBER_FUNCTION()
+
+template<
+    class T,
+    bool = ::sake_abs_private::is_callable< T ( T const & ) >::value
+>
+struct dispatch_on_is_callable;
+
+template<
+    class T,
+    bool = is_callable_mem_fun< T const &, T ( ) >::value
+>
+struct dispatch_on_is_callable_mem_fun;
+
+template<
+    class T,
+    bool = boost::is_signed<T>::value,
+    bool = boost::is_unsigned<T>::value
+>
+struct dispatch_on_is_signed_unsigned;
+
+template< class T >
+struct impl
+    : dispatch_on_is_callable<T>
+{ };
+
+template< class T >
+struct dispatch_on_is_callable< T, true >
+{
+    typedef T type;
+    static type apply(T const & x)
+    { return ::sake_abs_private::adl(x); }
+};
+
+template< class T >
+struct dispatch_on_is_callable< T, false >
+    : dispatch_on_is_callable_mem_fun<T>
+{ };
+
+template< class T >
+struct dispatch_on_is_callable_mem_fun< T, true >
+{
+    typedef T type;
+    static type apply(T const & x)
+    { return x.abs(); }
+};
+
+template< class T >
+struct dispatch_on_is_callable_mem_fun< T, false >
+    : dispatch_on_is_signed_unsigned<T>
+{ };
+
+template< class T >
+struct dispatch_on_is_signed_unsigned< T, true, false >
+{
+    typedef typename boost::make_unsigned<T>::type type;
+    static type apply(T const x)
+    {
+#ifdef _MSC_VER
+#pragma warning( push )
+#pragma warning( disable : 4146 ) // unary minus operator applied to unsigned type, result still unsigned
+#endif // #ifdef _MSC_VER
+        return x < 0 ? -static_cast< type >(x) : static_cast< type >(x);
+#ifdef _MSC_VER
+#pragma warning( pop )
+#endif // #ifdef _MSC_VER
+    }
+};
+
+template< class T >
+struct dispatch_on_is_signed_unsigned< T, false, true >
+{
+    typedef T type;
+    static type apply(T const x)
+    { return x; }
+};
+
+template< class T >
+struct dispatch_on_is_signed_unsigned< T, false, false >
+{
+    typedef typename boost_ext::common_type<
+        typename operators::result_of::unary_minus<T>::type, T >::type type;
+    static type apply(T const & x)
+    { return x < sake::zero ? -x : x; }
+};
+
+} // namespace abs_private
 
 } // namespace sake
 
