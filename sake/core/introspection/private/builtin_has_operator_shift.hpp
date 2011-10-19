42c0275b1cf383d26a62bd728fac766f6ca2750b
Jeffrey Hellrung <jeffrey.hellrung@gmail.com>
2011-10-19 12:04:06 -0700

    fixing copyright year

--- sake/core/introspection/private/builtin_has_operator_shift.hpp
+++ sake/core/introspection/private/builtin_has_operator_shift.hpp
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * sake/core/introspection/private/builtin_has_operator_shift.hpp
  *
- * Copyright 2010, Jeffrey Hellrung.
+ * Copyright 2011, Jeffrey Hellrung.
  * Distributed under the Boost Software License, Version 1.0.  (See accompanying
  * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  ******************************************************************************/
