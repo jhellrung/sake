cc403dac9b1d9696f581dc0cffb54a2dd7844aba
Jeffrey Hellrung <jeffrey.hellrung@gmail.com>
2011-10-31 12:02:17 -0700

    rewriting boost_ext::mpl::find_index[_if] in an attempt to reduce the number of template instantiations

--- sake/boost_ext/mpl/find_index.hpp
+++ sake/boost_ext/mpl/find_index.hpp
@@ -15,8 +15,8 @@
 #define SAKE_BOOST_EXT_MPL_FIND_INDEX_HPP
 
 #include <boost/mpl/begin_end.hpp>
-#include <boost/mpl/distance.hpp>
-#include <boost/mpl/find.hpp>
+#include <boost/mpl/deref.hpp>
+#include <boost/mpl/next.hpp>
 
 namespace sake
 {
@@ -27,11 +27,50 @@ namespace boost_ext
 namespace mpl
 {
 
+namespace find_index_private
+{
+
+template< class I, class E, class T, int K = 0 >
+struct iterate0;
+
+template<
+    class I, class E, class T, int K,
+    class U = typename boost::mpl::deref<I>::type
+>
+struct iterate1;
+
+template< class I, class E, class T, int K >
+struct iterate0
+    : iterate1<I,E,T,K>
+{ };
+
+template< class E, class T, int K >
+struct iterate0<E,E,T,K>
+{
+    static int const value = K;
+    typedef iterate0 type;
+};
+
+template< class I, class E, class T, int K, class U >
+struct iterate1
+    : iterate0< typename boost::mpl::next<I>::type, E, T, K+1 >
+{ };
+
+template< class I, class E, class T, int K >
+struct iterate1<I,E,T,K,T>
+{
+    static int const value = K;
+    typedef iterate1 type;
+};
+
+} // namespace find_index_private
+
 template< class Sequence, class T >
 struct find_index
-    : boost::mpl::distance<
+    : find_index_private::iterate0<
           typename boost::mpl::begin< Sequence >::type,
-          typename boost::mpl::find< Sequence, T >::type
+          typename boost::mpl::end< Sequence >::type,
+          T
       >
 { };
 
--- sake/boost_ext/mpl/find_index_if.hpp
+++ sake/boost_ext/mpl/find_index_if.hpp
@@ -14,9 +14,10 @@
 #ifndef SAKE_BOOST_EXT_MPL_FIND_INDEX_IF_HPP
 #define SAKE_BOOST_EXT_MPL_FIND_INDEX_IF_HPP
 
+#include <boost/mpl/apply.hpp>
 #include <boost/mpl/begin_end.hpp>
-#include <boost/mpl/distance.hpp>
-#include <boost/mpl/find_if.hpp>
+#include <boost/mpl/deref.hpp>
+#include <boost/mpl/next.hpp>
 
 namespace sake
 {
@@ -27,11 +28,50 @@ namespace boost_ext
 namespace mpl
 {
 
+namespace find_index_if_private
+{
+
+template< class I, class E, class F, int K = 0 >
+struct iterate0;
+
+template<
+    class I, class E, class F, int K,
+    bool = boost::mpl::apply1< F, typename boost::mpl::deref<I>::type >::type::value
+>
+struct iterate1;
+
+template< class I, class E, class T, int K >
+struct iterate0
+    : iterate1<I,E,T,K>
+{ };
+
+template< class E, class T, int K >
+struct iterate0<E,E,T,K>
+{
+    static int const value = K;
+    typedef iterate0 type;
+};
+
+template< class I, class E, class T, int K, class U >
+struct iterate1< I,E,T,K, false >
+    : iterate0< typename boost::mpl::next<I>::type, E, T, K+1 >
+{ };
+
+template< class I, class E, class T, int K >
+struct iterate1< I,E,T,K, true >
+{
+    static int const value = K;
+    typedef iterate1 type;
+};
+
+} // namespace find_index_if_private
+
 template< class Sequence, class F >
 struct find_index_if
-    : boost::mpl::distance<
+    : find_index_if_private::iterate0<
           typename boost::mpl::begin< Sequence >::type,
-          typename boost::mpl::find_if< Sequence, F >::type
+          typename boost::mpl::end< Sequence >::type,
+          F
       >
 { };
 
