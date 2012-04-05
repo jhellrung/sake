9f9e81855acf2e028a27a41033d6e117f9d1039b
Jeffrey Hellrung <jeffrey.hellrung@gmail.com>
2012-04-05 14:40:58 -0700

    simplifying implementation of has_isc

--- sake/core/introspection/has_isc.hpp
+++ sake/core/introspection/has_isc.hpp
@@ -32,10 +32,15 @@
 #ifndef SAKE_CORE_INTROSPECTION_HAS_ISC_HPP
 #define SAKE_CORE_INTROSPECTION_HAS_ISC_HPP
 
+#include <cstddef>
+
 #include <boost/mpl/always.hpp>
 #include <boost/mpl/apply.hpp>
+#include <boost/static_assert.hpp>
 #include <boost/type_traits/integral_constant.hpp>
+#include <boost/type_traits/is_void.hpp>
 
+#include <sake/core/expr_traits/integral_typeof.hpp>
 #include <sake/core/utility/yes_no_tag.hpp>
 
 #define SAKE_INTROSPECTION_DEFINE_HAS_ISC( trait, name ) \
@@ -45,20 +50,20 @@ class trait \
     template< int > struct sfinae; \
     template< class T_ > static ::sake::yes_tag test(sfinae< T_::name >*); \
     template< class T_ > static ::sake::no_tag test(...); \
-    static bool const has = sizeof( ::sake::yes_tag ) == sizeof( test<T>(0) ); \
-    template< class T_, bool > struct apply_pred; \
+    template< class T_, std::size_t > struct apply_pred; \
     template< class T_ > \
-    struct apply_pred< T_, true > \
+    struct apply_pred< T_, SAKE_SIZEOF_YES_TAG > \
     { \
-        SAKE_EXPR_INTEGRAL_TYPEOF_TYPEDEF( typename T_::name, type ); \
-        static bool const value = ::boost::mpl::apply1< \
-            Pred, ::boost::integral_constant< type, T_::name > >::type::value; \
+        SAKE_EXPR_INTEGRAL_TYPEOF_TYPEDEF( typename false || T_::name, type ); \
+        BOOST_STATIC_ASSERT((!boost::is_void< type >::value)); \
+        typedef ::boost::integral_constant< type, T_::name > wrapped_type; \
+        static bool const value = ::boost::mpl::apply1< Pred, wrapped_type >::type::value; \
     }; \
     template< class T_ > \
-    struct apply_pred< T_, false > \
+    struct apply_pred< T_, SAKE_SIZEOF_NO_TAG > \
     { static bool const value = false; }; \
 public: \
-    static bool const value = apply_pred< T, has >::value; \
+    static bool const value = apply_pred< T, sizeof( test<T>(0) ) >::value; \
     typedef trait type; \
 };
 
