f9eb4b8dde76bc44f43dcf9d19d87ff8abbd3cfb
Jeffrey Hellrung <jeffrey.hellrung@gmail.com>
2012-04-05 14:37:02 -0700

    simplifying implementation of SAKE_BOOST_EXT_PP_SEQ_SIZE

--- sake/boost_ext/preprocessor/seq/size.hpp
+++ sake/boost_ext/preprocessor/seq/size.hpp
@@ -14,17 +14,10 @@
 #ifndef SAKE_BOOST_EXT_PREPROCESSOR_SEQ_SIZE_HPP
 #define SAKE_BOOST_EXT_PREPROCESSOR_SEQ_SIZE_HPP
 
-#include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/arithmetic/dec.hpp>
 #include <boost/preprocessor/seq/size.hpp>
 
-#include <sake/boost_ext/preprocessor/seq/is_nil.hpp>
-
 #define SAKE_BOOST_EXT_PP_SEQ_SIZE( seq ) \
-    BOOST_PP_CAT( \
-        SAKE_BOOST_EXT_PP_SEQ_SIZE_, \
-        SAKE_BOOST_EXT_PP_SEQ_IS_NIL( seq ) \
-    ) ( seq )
-#define SAKE_BOOST_EXT_PP_SEQ_SIZE_0( seq ) BOOST_PP_SEQ_SIZE( seq )
-#define SAKE_BOOST_EXT_PP_SEQ_SIZE_1( seq ) 0
+    BOOST_PP_DEC( BOOST_PP_SEQ_SIZE( seq ( ~ ) ) )
 
 #endif // #ifndef SAKE_BOOST_EXT_PREPROCESSOR_SEQ_SIZE_HPP
