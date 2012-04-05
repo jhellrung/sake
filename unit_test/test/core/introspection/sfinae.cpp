1270ae26b4e863829f91e574fc0f936b71fb044f
Jeffrey Hellrung <jeffrey.hellrung@gmail.com>
2012-04-04 23:55:17 -0700

    minor edit

--- unit_test/test/core/introspection/sfinae.cpp
+++ unit_test/test/core/introspection/sfinae.cpp
@@ -27,10 +27,10 @@ struct X_type { struct x; };
 struct X_isc { static int const x = 0; };
 struct X_class_template { template< class > struct x; };
 struct X_mem_obj { int x; };
-struct X_mem_fn { void x(); };
-struct X_mem_fn_template { template< class T > void x(T); };
-struct X_static_mem_fn { static void x(); };
-struct X_static_mem_fn_template { template< class T > static void x(T); };
+struct X_mem_fun { void x(); };
+struct X_mem_fun_template { template< class T > void x(T); };
+struct X_static_mem_fun { static void x(); };
+struct X_static_mem_fun_template { template< class T > static void x(T); };
 
 template< class T >
 struct has_member
@@ -43,15 +43,15 @@ struct has_member
     static bool const value = sizeof( yes_tag ) == sizeof( test< derived >(0) );
 };
 
-BOOST_STATIC_ASSERT(!(has_member< X                        >::value));
-BOOST_STATIC_ASSERT( (has_member< X_type                   >::value));
-BOOST_STATIC_ASSERT( (has_member< X_isc                    >::value));
-BOOST_STATIC_ASSERT( (has_member< X_class_template         >::value));
-BOOST_STATIC_ASSERT( (has_member< X_mem_obj                >::value));
-BOOST_STATIC_ASSERT( (has_member< X_mem_fn                 >::value));
-BOOST_STATIC_ASSERT( (has_member< X_mem_fn_template        >::value));
-BOOST_STATIC_ASSERT( (has_member< X_static_mem_fn          >::value));
-BOOST_STATIC_ASSERT( (has_member< X_static_mem_fn_template >::value));
+BOOST_STATIC_ASSERT(!(has_member< X                         >::value));
+BOOST_STATIC_ASSERT( (has_member< X_type                    >::value));
+BOOST_STATIC_ASSERT( (has_member< X_isc                     >::value));
+BOOST_STATIC_ASSERT( (has_member< X_class_template          >::value));
+BOOST_STATIC_ASSERT( (has_member< X_mem_obj                 >::value));
+BOOST_STATIC_ASSERT( (has_member< X_mem_fun                 >::value));
+BOOST_STATIC_ASSERT( (has_member< X_mem_fun_template        >::value));
+BOOST_STATIC_ASSERT( (has_member< X_static_mem_fun          >::value));
+BOOST_STATIC_ASSERT( (has_member< X_static_mem_fun_template >::value));
 
 template< class T >
 struct has_type
@@ -62,15 +62,15 @@ struct has_type
     static bool const value = sizeof( yes_tag ) == sizeof( test<T>(0) );
 };
 
-BOOST_STATIC_ASSERT(!(has_type< X                        >::value));
-BOOST_STATIC_ASSERT( (has_type< X_type                   >::value));
-BOOST_STATIC_ASSERT(!(has_type< X_isc                    >::value));
-BOOST_STATIC_ASSERT(!(has_type< X_class_template         >::value));
-BOOST_STATIC_ASSERT(!(has_type< X_mem_obj                >::value));
-BOOST_STATIC_ASSERT(!(has_type< X_mem_fn                 >::value));
-BOOST_STATIC_ASSERT(!(has_type< X_mem_fn_template        >::value));
-BOOST_STATIC_ASSERT(!(has_type< X_static_mem_fn          >::value));
-BOOST_STATIC_ASSERT(!(has_type< X_static_mem_fn_template >::value));
+BOOST_STATIC_ASSERT(!(has_type< X                         >::value));
+BOOST_STATIC_ASSERT( (has_type< X_type                    >::value));
+BOOST_STATIC_ASSERT(!(has_type< X_isc                     >::value));
+BOOST_STATIC_ASSERT(!(has_type< X_class_template          >::value));
+BOOST_STATIC_ASSERT(!(has_type< X_mem_obj                 >::value));
+BOOST_STATIC_ASSERT(!(has_type< X_mem_fun                 >::value));
+BOOST_STATIC_ASSERT(!(has_type< X_mem_fun_template        >::value));
+BOOST_STATIC_ASSERT(!(has_type< X_static_mem_fun          >::value));
+BOOST_STATIC_ASSERT(!(has_type< X_static_mem_fun_template >::value));
 
 template< class T >
 struct has_isc
@@ -81,15 +81,15 @@ struct has_isc
     static bool const value = sizeof( yes_tag ) == sizeof( test<T>(0) );
 };
 
-BOOST_STATIC_ASSERT(!(has_isc< X                        >::value));
-BOOST_STATIC_ASSERT(!(has_isc< X_type                   >::value));
-BOOST_STATIC_ASSERT( (has_isc< X_isc                    >::value));
-BOOST_STATIC_ASSERT(!(has_isc< X_class_template         >::value));
-//BOOST_STATIC_ASSERT(!(has_isc< X_mem_obj                >::value)); // !GNUC
-//BOOST_STATIC_ASSERT(!(has_isc< X_mem_fn                 >::value)); // !GNUC
-BOOST_STATIC_ASSERT(!(has_isc< X_mem_fn_template        >::value));
-BOOST_STATIC_ASSERT(!(has_isc< X_static_mem_fn          >::value));
-BOOST_STATIC_ASSERT(!(has_isc< X_static_mem_fn_template >::value));
+BOOST_STATIC_ASSERT(!(has_isc< X                         >::value));
+BOOST_STATIC_ASSERT(!(has_isc< X_type                    >::value));
+BOOST_STATIC_ASSERT( (has_isc< X_isc                     >::value));
+BOOST_STATIC_ASSERT(!(has_isc< X_class_template          >::value));
+//BOOST_STATIC_ASSERT(!(has_isc< X_mem_obj                 >::value)); // !GNUC
+//BOOST_STATIC_ASSERT(!(has_isc< X_mem_fun                 >::value)); // !GNUC
+BOOST_STATIC_ASSERT(!(has_isc< X_mem_fun_template        >::value));
+BOOST_STATIC_ASSERT(!(has_isc< X_static_mem_fun          >::value));
+BOOST_STATIC_ASSERT(!(has_isc< X_static_mem_fun_template >::value));
 
 template< class T >
 struct has_class_template
@@ -100,15 +100,15 @@ struct has_class_template
     static bool const value = sizeof( yes_tag ) == sizeof( test<T>(0) );
 };
 
-BOOST_STATIC_ASSERT(!(has_class_template< X                        >::value));
-BOOST_STATIC_ASSERT(!(has_class_template< X_type                   >::value));
-BOOST_STATIC_ASSERT(!(has_class_template< X_isc                    >::value));
-BOOST_STATIC_ASSERT( (has_class_template< X_class_template         >::value));
-BOOST_STATIC_ASSERT(!(has_class_template< X_mem_obj                >::value));
-BOOST_STATIC_ASSERT(!(has_class_template< X_mem_fn                 >::value));
-BOOST_STATIC_ASSERT(!(has_class_template< X_mem_fn_template        >::value));
-BOOST_STATIC_ASSERT(!(has_class_template< X_static_mem_fn          >::value));
-BOOST_STATIC_ASSERT(!(has_class_template< X_static_mem_fn_template >::value));
+BOOST_STATIC_ASSERT(!(has_class_template< X                         >::value));
+BOOST_STATIC_ASSERT(!(has_class_template< X_type                    >::value));
+BOOST_STATIC_ASSERT(!(has_class_template< X_isc                     >::value));
+BOOST_STATIC_ASSERT( (has_class_template< X_class_template          >::value));
+BOOST_STATIC_ASSERT(!(has_class_template< X_mem_obj                 >::value));
+BOOST_STATIC_ASSERT(!(has_class_template< X_mem_fun                 >::value));
+BOOST_STATIC_ASSERT(!(has_class_template< X_mem_fun_template        >::value));
+BOOST_STATIC_ASSERT(!(has_class_template< X_static_mem_fun          >::value));
+BOOST_STATIC_ASSERT(!(has_class_template< X_static_mem_fun_template >::value));
 
 template< class T >
 struct has_mem_obj
@@ -119,14 +119,14 @@ struct has_mem_obj
     static bool const value = sizeof( yes_tag ) == sizeof( test<T>(0) );
 };
 
-BOOST_STATIC_ASSERT(!(has_mem_obj< X                        >::value));
-BOOST_STATIC_ASSERT(!(has_mem_obj< X_type                   >::value));
-//BOOST_STATIC_ASSERT(!(has_mem_obj< X_isc                    >::value)); // !MSVC, !GNUC
-BOOST_STATIC_ASSERT(!(has_mem_obj< X_class_template         >::value));
-BOOST_STATIC_ASSERT( (has_mem_obj< X_mem_obj                >::value));
-BOOST_STATIC_ASSERT(!(has_mem_obj< X_mem_fn                 >::value));
-//BOOST_STATIC_ASSERT(!(has_mem_obj< X_mem_fn_template        >::value)); // !GNUC
-//BOOST_STATIC_ASSERT(!(has_mem_obj< X_static_mem_fn          >::value)); // !GNUC
-//BOOST_STATIC_ASSERT(!(has_mem_obj< X_static_mem_fn_template >::value)); // !GNUC
+BOOST_STATIC_ASSERT(!(has_mem_obj< X                         >::value));
+BOOST_STATIC_ASSERT(!(has_mem_obj< X_type                    >::value));
+//BOOST_STATIC_ASSERT(!(has_mem_obj< X_isc                     >::value)); // !MSVC, !GNUC
+BOOST_STATIC_ASSERT(!(has_mem_obj< X_class_template          >::value));
+BOOST_STATIC_ASSERT( (has_mem_obj< X_mem_obj                 >::value));
+BOOST_STATIC_ASSERT(!(has_mem_obj< X_mem_fun                 >::value));
+//BOOST_STATIC_ASSERT(!(has_mem_obj< X_mem_fun_template        >::value)); // !GNUC
+//BOOST_STATIC_ASSERT(!(has_mem_obj< X_static_mem_fun          >::value)); // !GNUC
+//BOOST_STATIC_ASSERT(!(has_mem_obj< X_static_mem_fun_template >::value)); // !GNUC
 
 } // namespace
