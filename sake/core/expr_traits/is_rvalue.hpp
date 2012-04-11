8121d3e5bf662def9a30a820e6e1c85ee8b55cfa
Jeffrey Hellrung <jeffrey.hellrung@gmail.com>
2012-04-10 18:32:37 -0700

    updating copyright date

--- sake/core/expr_traits/is_rvalue.hpp
+++ sake/core/expr_traits/is_rvalue.hpp
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * sake/core/expr_traits/is_rvalue.hpp
  *
- * Copyright 2011, Jeffrey Hellrung.
+ * Copyright 2012, Jeffrey Hellrung.
  * Distributed under the Boost Software License, Version 1.0.  (See accompanying
  * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  *
