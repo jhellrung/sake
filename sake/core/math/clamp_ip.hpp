040950148ed1fc280db914a76ac180a0c46a00be
Jeffrey Hellrung <jeffrey.hellrung@gmail.com>
2012-04-11 10:04:44 -0700

    mostly cosmetic changes

--- sake/core/math/clamp_ip.hpp
+++ sake/core/math/clamp_ip.hpp
@@ -17,7 +17,7 @@
 
 #include <boost/static_assert.hpp>
 
-#include <sake/boost_ext/type_traits/is_lvalue_reference.hpp>
+#include <sake/boost_ext/type_traits/is_lvalue_reference_to_nonconst.hpp>
 
 #include <sake/core/move/forward.hpp>
 #include <sake/core/math/unbounded_tag.hpp>
@@ -33,7 +33,7 @@ namespace result_of
 template< class L, class T, class U, class Less = void >
 struct clamp_ip
 {
-    BOOST_STATIC_ASSERT((boost_ext::is_lvalue_reference<T>::value));
+    BOOST_STATIC_ASSERT((boost_ext::is_lvalue_reference_to_nonconst<T>::value));
     typedef T type;
 };
 
@@ -47,49 +47,41 @@ struct clamp_ip
     SAKE_RESULT_FROM_METAFUNCTION( result_of::clamp_ip, (3,4) )
 
     template< class L, class T, class U >
-    T&
-    operator()(SAKE_FWD_REF( L ) lower, T& x, SAKE_FWD_REF( U ) upper) const
+    T& operator()(SAKE_FWD_REF( L ) lower, T& x, SAKE_FWD_REF( U ) upper) const
     {
         SAKE_ASSERT(!(upper < lower));
         return !(upper < x) ? !(x < lower) ? x : x = sake::forward<L>(lower) : x = sake::forward<U>(upper);
     }
 
     template< class L, class T, class U, class Less >
-    T&
-    operator()(SAKE_FWD_REF( L ) lower, T& x, SAKE_FWD_REF( U ) upper, Less less) const
+    T& operator()(SAKE_FWD_REF( L ) lower, T& x, SAKE_FWD_REF( U ) upper, Less less) const
     {
         SAKE_ASSERT(!less(upper, lower));
         return !less(upper, x) ? !less(x, lower) ? x : x = sake::forward<L>(lower) : x = sake::forward<U>(upper);
     }
 
     template< class L, class T >
-    T&
-    operator()(SAKE_FWD_REF( L ) lower, T& x, sake::unbounded_tag) const
+    T& operator()(SAKE_FWD_REF( L ) lower, T& x, sake::unbounded_tag) const
     { return !(x < lower) ? x : x = sake::forward<L>(lower); }
 
     template< class L, class T, class Less >
-    T&
-    operator()(SAKE_FWD_REF( L ) lower, T& x, sake::unbounded_tag, Less less) const
+    T& operator()(SAKE_FWD_REF( L ) lower, T& x, sake::unbounded_tag, Less less) const
     { return !less(x, lower) ? x : x = sake::forward<L>(lower); }
 
     template< class T, class U >
-    T&
-    operator()(sake::unbounded_tag, T& x, SAKE_FWD_REF( U ) upper) const
+    T& operator()(sake::unbounded_tag, T& x, SAKE_FWD_REF( U ) upper) const
     { return !(upper < x) ? x : x = sake::forward<U>(upper); }
 
     template< class T, class U, class Less >
-    T&
-    operator()(sake::unbounded_tag, T& x, SAKE_FWD_REF( U ) upper, Less less) const
+    T& operator()(sake::unbounded_tag, T& x, SAKE_FWD_REF( U ) upper, Less less) const
     { return !less(upper, x) ? x : x = sake::forward<U>(upper); }
 
     template< class T >
-    T&
-    operator()(sake::unbounded_tag, T& x, sake::unbounded_tag) const
+    T& operator()(sake::unbounded_tag, T& x, sake::unbounded_tag) const
     { return x; }
 
     template< class T, class Less >
-    T&
-    operator()(sake::unbounded_tag, T& x, sake::unbounded_tag, Less const & /*less*/) const
+    T& operator()(sake::unbounded_tag, T& x, sake::unbounded_tag, Less const & /*less*/) const
     { return x; }
 };
 
