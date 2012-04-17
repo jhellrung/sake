1db36592d834ce01bbc65312de66a5653c4338fe
Jeffrey Hellrung <jeffrey.hellrung@gmail.com>
2012-04-17 13:41:52 -0700

    removing hash_value and swap from sake::void_...that was a mistake

--- sake/core/utility/void.hpp
+++ sake/core/utility/void.hpp
@@ -15,20 +15,12 @@
 #ifndef SAKE_CORE_UTILITY_VOID_HPP
 #define SAKE_CORE_UTILITY_VOID_HPP
 
-#include <cstddef>
-
 #include <boost/type_traits/integral_constant.hpp>
 
 namespace sake
 {
 
-struct void_
-{
-    static void swap(void_ const &)
-    { }
-    static std::size_t hash_value()
-    { return 0; }
-};
+struct void_ { };
 
 template< class T >
 struct is_void_
