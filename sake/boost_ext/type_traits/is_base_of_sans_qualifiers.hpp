0b20e50f293bb734161c900c9783673ed262ef21
Jeffrey Hellrung <jeffrey.hellrung@gmail.com>
2012-04-19 12:48:04 -0700

    ensuring that is_base_of_sans_qualifiers<T,T> is true

--- sake/boost_ext/type_traits/is_base_of_sans_qualifiers.hpp
+++ sake/boost_ext/type_traits/is_base_of_sans_qualifiers.hpp
@@ -11,6 +11,7 @@
 #ifndef SAKE_BOOST_EXT_TYPE_TRAITS_IS_BASE_OF_SANS_QUALIFIERS_HPP
 #define SAKE_BOOST_EXT_TYPE_TRAITS_IS_BASE_OF_SANS_QUALIFIERS_HPP
 
+#include <boost/type_traits/integral_constant.hpp>
 #include <boost/type_traits/is_base_of.hpp>
 
 #include <sake/boost_ext/type_traits/remove_qualifiers.hpp>
@@ -21,9 +22,24 @@ namespace sake
 namespace boost_ext
 {
 
+namespace is_base_of_sans_qualifiers_private
+{
+
+template< class B, class D >
+struct dispatch
+    : boost::is_base_of<B,D>
+{ };
+
+template< class B >
+struct dispatch<B,B>
+    : boost::true_type
+{ };
+
+} // namespace is_base_of_sans_qualifiers_private
+
 template< class B, class D >
 struct is_base_of_sans_qualifiers
-    : boost::is_base_of<
+    : is_base_of_sans_qualifiers_private::dispatch<
           typename boost_ext::remove_qualifiers<B>::type,
           typename boost_ext::remove_qualifiers<D>::type
       >
