74f0bf4d4a84d128d1fd705aba6af5ea3b4c5f9d
Jeffrey Hellrung <jeffrey.hellrung@gmail.com>
2012-04-20 23:27:11 -0700

    forgot that boost::is_base_of<T,T> already returns true...

--- sake/boost_ext/type_traits/is_base_of_sans_qualifiers.hpp
+++ sake/boost_ext/type_traits/is_base_of_sans_qualifiers.hpp
@@ -6,12 +6,14 @@
  * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  *
  * struct boost_ext::is_base_of_sans_qualifiers<B,D>
+ *
+ * Note that, as for boost::is_base_of, this returns true if B and D are the
+ * same class type (sans qualifiers).
  ******************************************************************************/
 
 #ifndef SAKE_BOOST_EXT_TYPE_TRAITS_IS_BASE_OF_SANS_QUALIFIERS_HPP
 #define SAKE_BOOST_EXT_TYPE_TRAITS_IS_BASE_OF_SANS_QUALIFIERS_HPP
 
-#include <boost/type_traits/integral_constant.hpp>
 #include <boost/type_traits/is_base_of.hpp>
 
 #include <sake/boost_ext/type_traits/remove_qualifiers.hpp>
@@ -22,24 +24,9 @@ namespace sake
 namespace boost_ext
 {
 
-namespace is_base_of_sans_qualifiers_private
-{
-
-template< class B, class D >
-struct dispatch
-    : boost::is_base_of<B,D>
-{ };
-
-template< class B >
-struct dispatch<B,B>
-    : boost::true_type
-{ };
-
-} // namespace is_base_of_sans_qualifiers_private
-
 template< class B, class D >
 struct is_base_of_sans_qualifiers
-    : is_base_of_sans_qualifiers_private::dispatch<
+    : boost::is_base_of<
           typename boost_ext::remove_qualifiers<B>::type,
           typename boost_ext::remove_qualifiers<D>::type
       >
