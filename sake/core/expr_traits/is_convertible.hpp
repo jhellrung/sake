1e6ee801224686a7cf54ce4ced68a8cfb3d493aa
Jeffrey Hellrung <jeffrey.hellrung@gmail.com>
2012-04-20 23:28:23 -0700

    explicitly disabling lvalue -> rvalue ref conversions in C++03

--- sake/core/expr_traits/is_convertible.hpp
+++ sake/core/expr_traits/is_convertible.hpp
@@ -17,12 +17,14 @@
 #ifndef SAKE_CORE_EXPR_TRAITS_IS_CONVERTIBLE_HPP
 #define SAKE_CORE_EXPR_TRAITS_IS_CONVERTIBLE_HPP
 
+#include <boost/config.hpp>
 #include <boost/static_assert.hpp>
 #include <boost/type_traits/is_object.hpp>
 
 #include <sake/boost_ext/mpl/or.hpp>
 #include <sake/boost_ext/type_traits/is_reference.hpp>
 
+#include <sake/core/move/rv.hpp>
 #include <sake/core/utility/declval.hpp>
 #include <sake/core/utility/true_false_tag.hpp>
 
@@ -50,6 +52,17 @@ struct helper< T, true >
     static sake::true_tag  apply(T);
     static sake::false_tag apply(...);
 };
+
+#ifdef BOOST_NO_RVALUE_REFERENCES
+// Explicitly disable any implicit T& -> boost::rv<T>& conversions.
+template< class T >
+struct helper< SAKE_RV_REF( T ), true >
+{
+    static sake::true_tag  apply(SAKE_RV_REF( T ));
+    static sake::false_tag apply(...);
+    static sake::false_tag apply(T&);
+};
+#endif // #ifdef BOOST_NO_RVALUE_REFERENCES
 
 template<>
 struct helper< void, false >
