56e251210b44f149c4492b1f7f5c4fbdb966783e
Jeffrey Hellrung <jeffrey.hellrung@gmail.com>
2012-04-25 21:54:00 -0700

    updating movable_test

--- unit_test/test/core/move/movable_test.cpp
+++ unit_test/test/core/move/movable_test.cpp
@@ -8,7 +8,6 @@
 
 #include <boost/config.hpp>
 
-#include <sake/core/functional/construct.hpp>
 #include <sake/core/move/move.hpp>
 
 #include <sake/test/environment.hpp>
@@ -43,85 +42,76 @@ void movable_test(sake::test::environment& env)
         SAKE_TEST_CHECK_RELATION( env, x.value, ==, 0 );
         SAKE_TEST_CHECK_RELATION( env, y.value, !=, 0 );
     }
+    models::special_mem_fun_stats stats;
     {
         typedef models::basic_movable_copyable<> type;
-        models::special_mem_fun_stats stats;
-        {
-            type x(stats);
-            type y(x);
-            type z(sake::move(y));
-            x = z;
-            y = sake::move(x);
-        }
-        SAKE_TEST_CHECK_RELATION( env, stats.n_other_constructor, ==, 1 );
-        SAKE_TEST_CHECK_RELATION( env, stats.n_copy_constructor, ==, 1 );
-        SAKE_TEST_CHECK_RELATION( env, stats.n_move_constructor, ==, 1 );
-        SAKE_TEST_CHECK_RELATION( env, stats.n_copy_assign, ==, 1 );
-        SAKE_TEST_CHECK_RELATION( env, stats.n_move_assign, ==, 1 );
-        SAKE_TEST_CHECK_RELATION( env, stats.n_destructor, ==, 3 );
+        type x(stats);
+        type y(x);
+        type z(sake::move(y));
+        x = z;
+        y = sake::move(x);
     }
+    SAKE_TEST_CHECK_RELATION( env, stats.n_other_constructor, ==, 1 );
+    SAKE_TEST_CHECK_RELATION( env, stats.n_copy_constructor, ==, 1 );
+    SAKE_TEST_CHECK_RELATION( env, stats.n_move_constructor, ==, 1 );
+    SAKE_TEST_CHECK_RELATION( env, stats.n_copy_assign, ==, 1 );
+    SAKE_TEST_CHECK_RELATION( env, stats.n_move_assign, ==, 1 );
+    SAKE_TEST_CHECK_RELATION( env, stats.n_destructor, ==, 3 );
+    stats.reset();
     {
         typedef models::optimal_movable_copyable<> type;
-        models::special_mem_fun_stats stats;
-        {
-            type x(stats);
-            type y(x);
-            type z(sake::move(y));
-            x = z;
-            y = sake::move(x);
-            z = sake::construct< type >(stats);
-        }
-        SAKE_TEST_CHECK_RELATION( env, stats.n_other_constructor, ==, 2 );
-        SAKE_TEST_CHECK_RELATION( env, stats.n_copy_constructor, ==, 1 );
-        SAKE_TEST_CHECK_RELATION( env, stats.n_move_constructor, >=, 1 );
-        SAKE_TEST_CHECK_RELATION( env, stats.n_copy_assign, ==, 1 );
-        SAKE_TEST_CHECK_RELATION( env, stats.n_move_assign, ==, 2 );
-        SAKE_TEST_CHECK_RELATION( env, stats.n_destructor, >=, 4 );
+        type x(stats);
+        type y(x);
+        type z(sake::move(y));
+        x = z;
+        y = sake::move(x);
+        z = type(stats);
     }
+    SAKE_TEST_CHECK_RELATION( env, stats.n_other_constructor, ==, 2 );
+    SAKE_TEST_CHECK_RELATION( env, stats.n_copy_constructor, ==, 1 );
+    SAKE_TEST_CHECK_RELATION( env, stats.n_move_constructor, ==, 1 );
+    SAKE_TEST_CHECK_RELATION( env, stats.n_copy_assign, ==, 1 );
+    SAKE_TEST_CHECK_RELATION( env, stats.n_move_assign, ==, 2 );
+    SAKE_TEST_CHECK_RELATION( env, stats.n_destructor, ==, 4 );
+    stats.reset();
     {
         typedef models::friendly_movable_copyable<> type;
-        models::special_mem_fun_stats stats;
-        {
-            type x(stats);
-            type y(x);
-            type z(sake::move(y));
-            x = z;
-            y = sake::move(x);
-            z = sake::construct< type >(stats);
-        }
-#ifndef BOOST_NO_RVALUE_REFERENCES
-        SAKE_TEST_CHECK_RELATION( env, stats.n_other_constructor, ==, 2 );
-        SAKE_TEST_CHECK_RELATION( env, stats.n_copy_constructor, ==, 1 );
-        SAKE_TEST_CHECK_RELATION( env, stats.n_move_constructor, >=, 1 );
-        SAKE_TEST_CHECK_RELATION( env, stats.n_copy_assign, ==, 1 );
-        SAKE_TEST_CHECK_RELATION( env, stats.n_move_assign, ==, 2 );
-        SAKE_TEST_CHECK_RELATION( env, stats.n_destructor, >=, 4 );
-#else // #ifndef BOOST_NO_RVALUE_REFERENCES
-        SAKE_TEST_CHECK_RELATION( env, stats.n_other_constructor, ==, 2 );
-        SAKE_TEST_CHECK_RELATION( env, stats.n_copy_constructor, ==, 2 );
-        SAKE_TEST_CHECK_RELATION( env, stats.n_move_constructor, >=, 1 );
-        SAKE_TEST_CHECK_RELATION( env, stats.n_copy_assign, ==, 0 );
-        SAKE_TEST_CHECK_RELATION( env, stats.n_move_assign, ==, 3 );
-        SAKE_TEST_CHECK_RELATION( env, stats.n_destructor, >=, 4 );
-#endif // #ifndef BOOST_NO_RVALUE_REFERENCES
+        type x(stats);
+        type y(x);
+        type z(sake::move(y));
+        x = z;
+        y = sake::move(x);
+        z = type(stats);
     }
+    SAKE_TEST_CHECK_RELATION( env, stats.n_other_constructor, ==, 2 );
+    SAKE_TEST_CHECK_RELATION( env, stats.n_move_constructor, ==, 1 );
+#ifndef BOOST_NO_RVALUE_REFERENCES
+    SAKE_TEST_CHECK_RELATION( env, stats.n_copy_constructor, ==, 1 );
+    SAKE_TEST_CHECK_RELATION( env, stats.n_copy_assign, ==, 1 );
+    SAKE_TEST_CHECK_RELATION( env, stats.n_move_assign, ==, 2 );
+    SAKE_TEST_CHECK_RELATION( env, stats.n_destructor, ==, 4 );
+#else // #ifndef BOOST_NO_RVALUE_REFERENCES
+    SAKE_TEST_CHECK_RELATION( env, stats.n_copy_constructor, ==, 2 );
+    SAKE_TEST_CHECK_RELATION( env, stats.n_copy_assign, ==, 0 );
+    SAKE_TEST_CHECK_RELATION( env, stats.n_move_assign, ==, 3 );
+    SAKE_TEST_CHECK_RELATION( env, stats.n_destructor, ==, 5 );
+#endif // #ifndef BOOST_NO_RVALUE_REFERENCES
+    stats.reset();
     {
         typedef models::movable_noncopyable<> type;
-        models::special_mem_fun_stats stats;
-        {
-            type x(stats);
-            type y(sake::move(x));
-            type z(sake::construct< type >(stats));
-            x = sake::move(z);
-            y = sake::construct< type >(stats);
-        }
-        SAKE_TEST_CHECK_RELATION( env, stats.n_other_constructor, ==, 3 );
-        SAKE_TEST_CHECK_RELATION( env, stats.n_copy_constructor, ==, 0 );
-        SAKE_TEST_CHECK_RELATION( env, stats.n_move_constructor, >=, 2 );
-        SAKE_TEST_CHECK_RELATION( env, stats.n_copy_assign, ==, 0 );
-        SAKE_TEST_CHECK_RELATION( env, stats.n_move_assign, ==, 2 );
-        SAKE_TEST_CHECK_RELATION( env, stats.n_destructor, >=, 5 );
+        type x(stats);
+        type y(sake::move(x));
+        type z((type(stats)));
+        x = sake::move(z);
+        y = type(stats);
     }
+    SAKE_TEST_CHECK_RELATION( env, stats.n_other_constructor, ==, 3 );
+    SAKE_TEST_CHECK_RELATION( env, stats.n_copy_constructor, ==, 0 );
+    SAKE_TEST_CHECK_RELATION( env, stats.n_move_constructor, ==, 2 );
+    SAKE_TEST_CHECK_RELATION( env, stats.n_copy_assign, ==, 0 );
+    SAKE_TEST_CHECK_RELATION( env, stats.n_move_assign, ==, 2 );
+    SAKE_TEST_CHECK_RELATION( env, stats.n_destructor, ==, 5 );
+    stats.reset();
 }
 
 } // namespace sake_unit_test
