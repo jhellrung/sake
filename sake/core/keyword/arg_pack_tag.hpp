c1d099173e7207fc4f854d072c9c0348de7eddcd
Jeffrey Hellrung <jeffrey.hellrung@gmail.com>
2012-06-15 20:25:42 -0700

    adding arg_pack_tag

---
+++ sake/core/keyword/arg_pack_tag.hpp
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * sake/core/keyword/arg_pack_tag.hpp
+ *
+ * Copyright 2012, Jeffrey Hellrung.
+ * Distributed under the Boost Software License, Version 1.0.  (See accompanying
+ * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ ******************************************************************************/
+
+#ifndef SAKE_CORE_KEYWORD_ARG_PACK_TAG_HPP
+#define SAKE_CORE_KEYWORD_ARG_PACK_TAG_HPP
+
+namespace sake
+{
+
+namespace keyword
+{
+
+struct arg_pack_tag { };
+
+} // namespace keyword
+
+} // namespace sake
+
+#endif // #ifndef SAKE_CORE_KEYWORD_ARG_PACK_TAG_HPP
