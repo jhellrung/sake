66a1cfefd552be14da6cb8d85e7d3e43ffd6ca02
Jeffrey Hellrung <jeffrey.hellrung@gmail.com>
2012-07-11 23:58:49 -0700

    adding iterator::basic::empty

--- sake/core/iterator/basic/empty.hpp
+++ sake/core/iterator/basic/empty.hpp
@@ -9,6 +9,23 @@
 #ifndef SAKE_CORE_ITERATOR_BASIC_EMPTY_HPP
 #define SAKE_CORE_ITERATOR_BASIC_EMPTY_HPP
 
+#include <boost/mpl/map/map10.hpp>
+#include <boost/static_assert.hpp>
+#include <boost/type_traits/integral_constant.hpp>
+
+#include <sake/boost_ext/type_traits/is_cv_or.hpp>
+#include <sake/boost_ext/type_traits/is_reference.hpp>
+
+#include <sake/core/iterator/basic/fwd.hpp>
+#include <sake/core/iterator/categories.hpp>
+#include <sake/core/iterator/core_access.hpp>
+#include <sake/core/iterator/facade.hpp>
+#include <sake/core/iterator/keyword.hpp>
+#include <sake/core/math/sign_t.hpp>
+#include <sake/core/math/zero.hpp>
+#include <sake/core/utility/assert.hpp>
+#include <sake/core/utility/using_typedef.hpp>
+
 namespace sake
 {
 
@@ -18,8 +35,103 @@ namespace iterator
 namespace basic
 {
 
+namespace empty_private
+{
+
 template< class T >
-class empty;
+struct traits;
+
+} // namespace empty_private
+
+template< class T >
+class empty
+    : public empty_private::traits<T>::facade_
+{
+    typedef empty_private::traits<T> traits_;
+    SAKE_USING_TYPEDEF( typename traits_, facade_ );
+public:
+
+    typedef boost::true_type has_trivial_default_constructor_tag;
+    typedef boost::true_type has_trivial_copy_constructor_tag;
+    typedef boost::true_type has_trivial_copy_assign_tag;
+    typedef boost::true_type has_trivial_destructor_tag;
+
+    SAKE_USING_TYPEDEF( typename facade_, reference );
+    SAKE_USING_TYPEDEF( typename facade_, difference_type );
+
+private:
+    friend class sake::iterator::core_access;
+
+    reference derived_dereference() const
+    {
+        SAKE_REQUIRE((false));
+        return *static_cast< T* >(0);
+    }
+
+    void derived_increment()
+    { SAKE_REQUIRE((false)); }
+    void derived_decrement()
+    { SAKE_REQUIRE((false)); }
+
+    template< class U >
+    bool derived_equal(U) const
+    { return true; }
+
+    void derived_advance_ip(difference_type const n)
+    { SAKE_ASSERT_RELATION( n, ==, sake::zero ); }
+    empty derived_advance(difference_type const n)
+    {
+        SAKE_ASSERT_RELATION( n, ==, sake::zero );
+        return empty();
+    }
+
+    template< class U >
+    difference_type derived_difference(U) const
+    { return static_cast< difference_type >(0); }
+
+    template< class U >
+    bool derived_less(U) const
+    { return false; }
+    template< class U >
+    bool derived_less_equal(U) const
+    { return true; }
+    template< class U >
+    sake::sign_t derived_cmp(U) const
+    { return sake::sign_t::zero_tag(); }
+
+    template< class Introterminal >
+    struct derived_relax
+    { typedef empty type; };
+
+    template< class U >
+    void derived_at_ip(U)
+    { }
+    template< class U, class Introterminal >
+    empty derived_at(U, Introterminal) const
+    { return empty(); }
+};
+
+namespace empty_private
+{
+
+template< class T >
+struct traits
+{
+    BOOST_STATIC_ASSERT((!boost_ext::is_reference<T>::value));
+    BOOST_STATIC_ASSERT((!boost_ext::is_cv_or<T>::value));
+    typedef sake::iterator::facade<
+        sake::iterator::basic::empty<T>,
+        boost::mpl::map3<
+            sake::iterator::keyword::value<T>,
+            sake::iterator::keyword::traversal<
+                boost::random_access_traversal_tag >,
+            sake::iterator::keyword::introterminal<
+                sake::begin_access_end_access_introterminal_tag >,
+        >
+    > facade_;
+};
+
+} // namespace empty_private
 
 } // namespace basic
 
